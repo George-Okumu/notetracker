3a6e41d6a39a1d32a879cf6547730e8fd715cd27
George-Okumu <gokumu12@gmail.com>
2022-07-01 09:55:57 +0300

    config

--- Gemfile
+++ Gemfile
@@ -15,6 +15,10 @@ gem "puma", "~> 5.0"
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 
+
+# Use Active Model has_secure_password
+gem 'bcrypt', '~> 3.1.7'
+
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 # gem "rack-cors"
 
@@ -27,4 +31,7 @@ group :development do
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
 end
+
+gem "active_model_serializers", "~> 0.10.12"
+
 
---
+++ note-tracker/src/components/App.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "../pages/Login";
+import NotesList from "../pages/NotesList";
+import NewNote from "../pages/NewNote";
+import Settings from "../pages/Settings"
+
+function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    // auto-login
+    fetch("/me").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+
+  if (!user) return <Login onLogin={setUser} />;
+
+  return (
+    <>
+      {/* <NavBar user={user} setUser={setUser} /> */}
+      <main>
+        <Routes>
+          <Route path="/new" element={<NewNote user={user} />} />
+          <Route path="/profile" element={<Settings user={user} />}/>
+          <Route path="/" element={<NotesList user={user} setUser={setUser} />}/>
+        </Routes>
+      </main>
+    </>
+  );
+}
+
+export default App;
